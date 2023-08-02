98d058af54abdc52c003e4b26086cbfd773cf643
John Edmondson <edmondsonj@gmail.com>
2023-08-02 10:05:15 -0700

    testing server vs client components

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Contact() {
-  return <p>contact</p>;
+  return (
+    <div>
+      <h1>Contact</h1>
+      <Link href="/">Home Page</Link>
+    </div>
+  );
 }
